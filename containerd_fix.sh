ef48e17443c8edddf4b68d983b3e4d739ff0a01b
Doggi <7159082+Doggi@users.noreply.github.com>
2025-11-13 22:40:53 +0100

    Add '-y' flag to containerd installation command

--- containerd_fix.sh
+++ containerd_fix.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-apt install containerd.io=1.7.28-1~debian.12~bookworm --allow-downgrades
+apt install containerd.io=1.7.28-1~debian.12~bookworm --allow-downgrades -y
 apt-mark hold containerd.io
 
 systemctl restart containerd
